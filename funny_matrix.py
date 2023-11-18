8accb500bb0c42141c2fcd0a5e0fa1b1630ed9ce
Kavli-Kaffel <imakii245@outlook.com>
2023-11-19 02:21:58 +0300

    Цифры ездят

--- funny_matrix.py
+++ funny_matrix.py
@@ -1,11 +1,19 @@
 def matrix_print(cicle_count: int):
     #print('\n'.join([' '.join(['1' if i == j else '0' for j in range(cicle_count)]) for i in range(cicle_count)]))
-    for i in range (cicle_count):
+    for k in range (cicle_count):
         for i in range (cicle_count):
-         if i%2 == 0:
-            print('1')
-         else:
-            print('0') 
+            for j in range(cicle_count):
+                if i%2 == 0:
+                    if i+k == j:
+                        print('1', end='')
+                    else: 
+                        print('0', end='') 
+                else:
+                    if i-k == j:
+                        print('1', end='')
+                    else: 
+                        print('0', end='') 
+            print('')
         print('\n')
 
 def get_matrix_size() -> str:
