7a437254877bd88d259e95c98cd58c89e193b4ab
Kavli-Kaffel <imakii245@outlook.com>
2023-11-19 00:23:34 +0300

    Рисуется матрица

--- funny_matrix.py
+++ funny_matrix.py
@@ -1,15 +1,13 @@
-def greeting(name: str):
-    print(f"Hello {name}!")
-
+def matrix_print(cicle_count: int):
+    print('\n'.join([' '.join(['1' if i == j else '0' for j in range(cicle_count)]) for i in range(cicle_count)]))
 
 def get_matrix_size() -> str:
-    return input("Введите размер матрицы:")
+    pass
 
 
 def main():
-    matrix_size = get_matrix_size()
-    greeting(matrix_size)
-
+    matrix_size = int(input("Введите размер матрицы: "))
+    matrix_print(matrix_size)
 
 if __name__ == "__main__":
     main()
