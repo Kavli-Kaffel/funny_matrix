c87f34c4f11365483e8d7d3eec6480602f87d0d1
Kavli-Kaffel <imakii245@outlook.com>
2023-11-19 12:49:34 +0300

    Ночное обновление

--- funny_matrix.py
+++ funny_matrix.py
@@ -1,15 +1,19 @@
 def matrix_print(cicle_count: int):
     #print('\n'.join([' '.join(['1' if i == j else '0' for j in range(cicle_count)]) for i in range(cicle_count)]))
-    for k in range (cicle_count):
-        for i in range (cicle_count):
-            for j in range(cicle_count):
-                if i%2 == 0:
-                    if i+k == j:
+    #step_nun номер шага
+    #line_nun номер строки
+    #el_num номер элемента
+
+    for step_nun in range (cicle_count):
+        for line_nun in range (cicle_count):
+            for el_num in range(cicle_count):
+                if line_nun % 2 == 0:
+                    if line_nun+step_nun == el_num:
                         print('1', end='')
-                    else: 
+                    else:
                         print('0', end='') 
                 else:
-                    if i-k == j:
+                    if line_nun-step_nun == el_num:
                         print('1', end='')
                     else: 
                         print('0', end='') 
