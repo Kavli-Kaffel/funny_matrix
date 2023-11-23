53a8d162b342f3d7185658bce8d2fb3d523a194d
Kavli-Kaffel <imakii245@outlook.com>
2023-11-23 23:07:54 +0300

    Refactoring

--- funny_matrix.py
+++ funny_matrix.py
@@ -1,25 +1,3 @@
-# def matrix_print(cicle_count: int):
-#     # print('\n'.join([' '.join(['1' if i == j else '0' for j in range(cicle_count)]) for i in range(cicle_count)]))
-#     # step_nun номер шага
-#     # line_nun номер строки
-#     # el_num номер элемента
-
-#     for step_nun in range(cicle_count):
-#         for line_nun in range(cicle_count):
-#             for el_num in range(cicle_count):
-#                 if line_nun % 2 == 0:
-#                     if line_nun + step_nun == el_num:
-#                         print("1", end="")
-#                     else:
-#                         print("0", end="")
-#                 else:
-#                     if line_nun - step_nun == el_num:
-#                         print("1", end="")
-#                     else:
-#                         print("0", end="")
-#             print("")
-#         print("\n")
-
 import time
 
 
@@ -27,32 +5,17 @@ def get_matrix_size() -> int:
     return int(input("Введите размер матрицы: "))
 
 
-def generate_ones(matrix_size: int):  # -> list[tuple[int, int]]:
+def generate_ones(matrix_size: int) -> list[tuple[int, int]]:
     """
     Генерация единиц в виде массива координат в матрице
     Генерация представления матрицы в виде массива координат единиц
     Пример
     [(0,0),(1,1),...(n,n)]
     """
-    matrix = [(i, i) for i in range(matrix_size)]
-    return matrix
+    return [(i, i) for i in range(matrix_size)]
 
 
 def transform_matrix(matrix: list[tuple[int, int]]):
-    # j=0
-    # for i in range(len(matrix)):
-    #     if j >= len(matrix)/2:
-    #         pass
-    #     if matrix[i][1] < len(matrix) - i - 1 and matrix[i][0] == i:
-    #         matrix[i] = (matrix[i][0], matrix[i][1] + 1)
-    #     elif matrix[i][1] == len(matrix) - i - 1 and matrix[i][0] < len(matrix) - i - 1:
-    #         matrix[i] = (matrix[i][0] + 1, matrix[i][1])
-    #     elif matrix[i][0] == len(matrix) - i - 1 and matrix[i][1] > i:
-    #         matrix[i] = (matrix[i][0], matrix[i][1] - 1)
-    #     elif matrix[i][1] == i and matrix[i][0] > i:
-    #         matrix[i] = (matrix[i][0] - 1, matrix[i][1])
-    #     j+=1
-
     j = 0
     for i in range(len(matrix)):
         if matrix[i][1] < len(matrix) - j - 1 and matrix[i][0] == j:
@@ -64,28 +27,24 @@ def transform_matrix(matrix: list[tuple[int, int]]):
         elif matrix[i][1] == j and matrix[i][0] > j:
             matrix[i] = (matrix[i][0] - 1, matrix[i][1])
 
-        if i < (len(matrix)) // 2 and len(matrix) % 2 != 0:
+        if (i < (len(matrix)) // 2 and len(matrix) % 2 != 0) or (i < (len(matrix) - 1) // 2 and len(matrix) % 2 == 0):
             j += 1
-        elif i >= (len(matrix)) // 2 and len(matrix) % 2 != 0:
+        elif (i >= (len(matrix)) // 2 and len(matrix) % 2 != 0) or (i >= (len(matrix)) // 2 and len(matrix) % 2 == 0):
             j -= 1
-        elif i < (len(matrix) - 1) // 2 and len(matrix) % 2 == 0:
-            j += 1
-        elif i >= (len(matrix)) // 2 and len(matrix) % 2 == 0:
-            j -= 1
-
     return matrix
-    # pass
 
 
 def print_matrix(matrix: list[tuple[int, int]]):
+    set_matrix = set(matrix)
     for i in range(len(matrix)):
         for j in range(len(matrix)):
-            if (i, j) in matrix:
+            if (i, j) in set_matrix:
                 print("1", end=" ")
             else:
                 print("0", end=" ")
         print("")
     print("")
+    # TODO Стирать и печатать
 
 
 def main():
@@ -95,7 +54,6 @@ def main():
         print_matrix(matrix)
         matrix = transform_matrix(matrix)
         time.sleep(1)
-
 
 if __name__ == "__main__":
     main()
